c273376e35a3be04a291c1ee31a1af495a26ebb4
nicholasgoodwin3 <nicholasgoodwin3@gmail.com>
2022-09-27 23:30:42 -0400

    edit 1

---
+++ prediction.js
@@ -0,0 +1,79 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+// OUTPUT
+// "tesla"
+// random produces any of the items in the list.
+
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//OUTPUT
+//produces an error because 'name' is undefined
+
+
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//OUTPUT
+// password logs 12345
+//hashedPassword logs undefined
+
+
+
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//OUTPUT 
+// logs TRUE
+// logs TRUE
+
+
+
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//OUTPUT
+// logs value
+// logs [1,5,1,8,3,3]
+// logs 1
+// logs 5
+
+//commit
