ccde1fb75c31cf168c916d216053a4be639eabc1
nicholasgoodwin3 <nicholasgoodwin3@gmail.com>
2022-10-21 23:08:34 -0400

    edit 3

---
+++ ToDoList/client/src/components/Display.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+const Display = (props) => {
+    const {todoList,setTodoList} = props;
+    const handleCompleted = (todo) => {
+        todo.markDelete = !todo.markedDelete;
+        setTodoList([...todoList]);
+    };
+    const styled = (markedDelete) => {
+        if (markedDelete === true) {
+            return "Finished"
+        }
+        else {
+            return "notFinished";
+        }
+    };
+    const deleteButton = (idFromBelow) => {
+        setTodoList(
+            todoList.filter((todo, index) => {
+            return todo.id !== idFromBelow;
+            })
+        );
+    };
+    return (
+        <div>
+            {todoList.map((todo, index) => (
+            <div className={styled(todo.markedDelete)} key={todo.id}>
+                {" "}
+                <p>{todo.content}</p>
+                <input type="checkbox" onChange={() => handleCompleted(todo)} />
+                <button onClick={() => deleteButton(todo.id)}>Delete</button>{" "}
+            </div>
+            ))}
+        </div>
+        );
+    };
+    
+    export default Display;
