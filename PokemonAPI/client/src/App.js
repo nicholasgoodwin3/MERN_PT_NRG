4b540a769a1a28b74a7c30f3d4831886d335c41b
nicholasgoodwin3 <nicholasgoodwin3@gmail.com>
2022-10-23 12:47:12 -0400

    edit1

---
+++ PokemonAPI/client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0') //pulled from pokeAPI documentation
+      .then(response => {
+        return response.json()
+      })
+      .then(response => {
+        setPokemon(response.results)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);  
+  return (
+    <div className="App" style={{ width: "200px", margin: "auto" }}>
+      <ul>
+        {
+          pokemon.map((pokemonObj, index)=>{
+            return (<li key={index}>{pokemonObj.name}</li>)
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
